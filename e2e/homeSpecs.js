bd46941e597fc8a93bff7bc4922bcdf4837e6b0e
Sancho Sebastine <sancho.sebastine@wnco.com>
2018-06-07 03:43:51 -0500

    fixing homePageSpecs

--- e2e/homeSpecs.js
+++ e2e/homeSpecs.js
@@ -5,8 +5,8 @@ var BROWSER_URL = require('./constant').BROWSER_URL;
 describe('home page', function() {
   it('should have the right title', function () {
     browser.url(BROWSER_URL);
-    browser.waitForVisible('.user-edit-button', 5000);
-    var result = browser.getTitle('foobar');
+    browser.waitForVisible('.table-responsive', 5000);
+    var result = browser.getTitle();
 
     assert.strictEqual(result, 'Employee Portal');
 
